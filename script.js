d6471f4e2974ccbb0bc37e31e170cfcf2aebaeea
Karthick32005 <727723eumt062@skcet.ac.in>
2025-08-28 19:37:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let bleDevice;
 let bleCharacteristic;
 let isBluetoothConnected = false;
 
-const SERVICE_UUID = '6E400001-B5A3-F393-E0A9-E50E24DCCA9E';
-const CHARACTERISTIC_UUID = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E';
+const SERVICE_UUID = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';         // Nordic UART Service
+const CHARACTERISTIC_UUID = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
 
 const statusDisplay = document.getElementById('status');
 
@@ -83,3 +83,4 @@ function directionToCommand(angle) {
     default: return 'S';
   }
 }
+
